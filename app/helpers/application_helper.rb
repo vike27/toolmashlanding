c411a4ef6750e86b09b47ba55638fefbb11cd7f2
Javier Noris <norisjavier@gmail.com>
2014-07-16 19:27:08 -0700

    before deployment ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Krowdprize"
+    base_title = "Toolmash"
     if page_title.empty?
       base_title
     else
